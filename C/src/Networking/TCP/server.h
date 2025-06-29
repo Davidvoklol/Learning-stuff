49f832946d4d17800424081ffa07d49252308fb7
Lobster <polyak.daavid@gmail.com>
2025-06-29 23:28:23 +0200

    ...

--- C/src/Networking/TCP/server.h
+++
@@ -1,7 +0,0 @@
-# pragma once
-
-void OnListen(int socket_fd);
-void OnConnect(int socket_fd);
-void OnRecive(int socket_fd, char* message);
-char* Send();
-void OnSent(int socket_fd, char* message);
